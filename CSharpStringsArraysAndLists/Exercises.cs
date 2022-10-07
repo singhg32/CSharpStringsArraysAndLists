1e36702d3d09524301007dd485834122beb980a0
NatalieDillon <112569645+NatalieDillon@users.noreply.github.com>
2022-10-07 10:45:36 +0100

    Add byte array exercises

--- CSharpStringsArraysAndLists/Exercises.cs
+++ CSharpStringsArraysAndLists/Exercises.cs
@@ -8,7 +8,7 @@ namespace CSharpStringsArraysAndLists
 {
     public class Exercises
     {
-        // The first three exercises can be procedures if you wish
+        // The first four exercises can be procedures if you wish
         // Please add appropriate calling code to Program.cs
 
         // Add tests as well as calling code for the exercises where you use functions
@@ -17,18 +17,22 @@ namespace CSharpStringsArraysAndLists
         // Create an array of doubles each of which holds three coordinates
         // Print these to the screen
 
-        // 2: Temperatures
+        // 2: Byte Arrays
+        // i) Create an array of bytes that represents the word hello. Convert this to a string and output the result.
+        // ii) Convert the word hello both in Chinese and in English to bytes. Output the byte array. 
+
+        // 3: Temperatures
         // Populate a list of doubles to represent daily temperatures over two weeks
         // Calculate and output the min, max and average temperatures over the time period
         // Sort the list in ascending order and print it out
 
-        // 3: Students
+        // 4: Students
         // Populate a list of student data with a firstname, surname and date of birth. Use a tuple.
         // Print the names of the oldest and youngest students
         // Print out how many students were born after 2010
         // Create a new list of strings with the full names of all the students and print this out     
 
-        // 4: Pig Latin
+        // 5: Pig Latin
         // Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
         // Leave punctuation marks untouched.
         // The cat sat on the mat. => heTay atcay noay hetay atmay.
@@ -37,7 +41,7 @@ namespace CSharpStringsArraysAndLists
             throw new NotImplementedException();
         }
 
-        // 5. Mexican wave
+        // 6. Mexican wave
         //  1.  The input string will always be lower case but maybe empty.
         //  2.  If the character in the string is whitespace then pass over it as if it was an empty seat
         // Example
@@ -47,7 +51,7 @@ namespace CSharpStringsArraysAndLists
             throw new NotImplementedException();
         }
 
-        // 6. Anagram
+        // 7. Anagram
         // Given a word and a list of words return the number of words that are anagrams of the others
         // Anagram("star", ["rats", "arts", "arc"]) => 2
         public static int Anagram(string input, string[] possibleAnagrams)
@@ -55,7 +59,7 @@ namespace CSharpStringsArraysAndLists
             throw new NotImplementedException();
         }
 
-        // 7. Variable Name helper
+        // 8. Variable Name helper
         public enum VariableNameType
         {
             CamelCase,
@@ -68,7 +72,7 @@ namespace CSharpStringsArraysAndLists
             throw new NotImplementedException();
         }
 
-        // 8. Binary search
+        // 9. Binary search
         // The function should take a sorted list. You can sort it in the calling code using default sort.
         // Use a binary search to find the requested value (write this yourself)
         // It should return the index of the value found or -1 if it isn't found
